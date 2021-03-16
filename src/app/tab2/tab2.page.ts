a8b50c408f22143b2b50f16e3110faf7ee1bba3d
hartantio <c14180134@john.petra.ac.id>
2021-03-16 19:59:41 +0700

    no2 &3

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FotoService } from '../services/foto.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,29 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
-
+  Rand = Math.floor(Math.random() * 10);
+  Angka
+  buener = false
+  constructor(public fotoService:FotoService) {}
+  ionTabsDidChange(){
+    this.buener=false;
+  }
+  ionTabsWillChange(){
+    this.buener=false;
+  }
+  Tebak(){
+    if(this.Angka== this.Rand){
+      this.fotoService.loadFoto()
+      this.buener=true
+      console.log("benar njir")
+    }
+    else{
+      console.log("sala")
+    }
+  }
+  Restart(){
+    this.buener=false
+    this.Rand = Math.floor(Math.random() * 10);
+  }
+  
 }
